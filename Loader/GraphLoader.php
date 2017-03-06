ef0e8e9bacf758656864e41ae76e8be6ea22daf2
Serg Ievsikov <esa1905@bigmir.net>
2017-03-06 13:51:19 +0200

    Second test commit

---
+++ Loader/GraphLoader.php
@@ -0,0 +1,14 @@
+<?php
+namespace Loader;
+/**
+ * Created by PhpStorm.
+ * User: SERG
+ * Date: 06.03.2017
+ * Time: 13:12
+ */
+class GraphLoader
+{
+    public static function test(){
+        return "test";
+    }
+}
